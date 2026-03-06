b612f1614ea66e3dd05d40a5c73cfc04bc1ff8ab
Hollymoly12 <tomy2699@gmail.com>
2026-03-07 00:04:13 +0100

    Add files via upload

---
+++ src/app/(auth)/auth-callback-route.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
+  const redirect = searchParams.get("redirect") || "/dashboard";
+
+  if (code) {
+    const supabase = await createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(new URL(redirect, req.url));
+}
